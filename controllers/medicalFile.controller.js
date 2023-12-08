bb34dd29cccc27bdab18f8144b9b9aca3ed9c80c
KamelHtira <Kamel.htira@isamm.u-manouba.tn>
2023-12-08 18:13:06 +0100

    get patients between 2 dates

--- controllers/medicalFile.controller.js
+++ controllers/medicalFile.controller.js
@@ -1,4 +1,5 @@
 const MedicalFile = require("../models/medicalFile");
+const Patient = require("../models/patient");
 
 const createMedicalFile = async (req, res) => {
   const newMedicalFile = new MedicalFile(req.body);
@@ -41,6 +42,53 @@ const getMedicalFileByPatientId = async (req, res) => {
     }
     res.send(MedicalFileToShow);
   } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getAllMedicalFilesBetweenTwoDates = async (req, res) => {
+  try {
+    const { from, to } = req.query;
+
+    if (!from || !to) {
+      return res.status(400).json({ error: 'Both from and to dates are required in the query parameters.' });
+    }
+
+    const fromDate = new Date(from);
+    const toDate = new Date(to);
+
+    if (isNaN(fromDate.getTime()) || isNaN(toDate.getTime())) {
+      return res.status(400).json({ error: 'Invalid date format. Please use ISO date format (e.g., YYYY-MM-DDTHH:mm:ss.sssZ).' });
+    }
+
+    // Find medical files between the specified dates
+    const medicalFiles = await MedicalFile.find({
+      createdAt: { $gte: fromDate, $lte: toDate },
+    });
+
+    // Extract patientIds from medicalFiles
+    const patientIds = medicalFiles.map(file => file.patientId);
+
+    // Find patients based on patientIds
+    const patients = await Patient.find({ _id: { $in: patientIds } });
+
+    // Create the final response object
+    const result = medicalFiles.map(file => {
+      const patient = patients.find(patient => patient._id.equals(file.patientId));
+
+      return {
+        medicalFileId: file._id,
+        patientId: file.patientId,
+        medicalFileDate: file.createdAt,
+        firstName: patient ? patient.firstName : null,
+        lastName: patient ? patient.lastName : null,
+        birthday: patient ? patient.birthday : null,
+      };
+    });
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
     res.status(500).send(error);
   }
 };
@@ -97,4 +145,5 @@ module.exports = {
   deleteMedicalFile,
   deleteMedicalFiles,
   getMedicalFileByPatientId,
+  getAllMedicalFilesBetweenTwoDates,
 };
